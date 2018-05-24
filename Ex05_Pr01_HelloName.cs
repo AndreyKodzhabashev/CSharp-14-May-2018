6559122a5e22fdf05894c145c1e24f0e8e0e654c
AndreyKodzhabashev <37802907+AndreyKodzhabashev@users.noreply.github.com>
2018-05-24 12:01:47 +0300

    Solved problems form Methods

---
+++ Ex05_Pr01_HelloName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex05_Pr01_HelloName
+{
+    class Ex05_Pr01_HelloName
+    {
+        static void Main()
+        {
+            GreetByName(Console.ReadLine());
+        }
+        static void GreetByName(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
